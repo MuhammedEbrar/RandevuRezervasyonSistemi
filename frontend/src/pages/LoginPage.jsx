d7a30a75b4baf36cec4789e4ff9bf3cfc5763be1
Fatih <fatihdurgut13@gmail.com>
2025-07-10 00:05:13 +0300

    feat: LoginPage'i ana uygulama bileşeni olarak göster

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Giriş Sayfası</h1>
+      <p>Burası giriş sayfası olacak.</p>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RegisterPage() {
+  return (
+    <div>
+      <h1>Kayıt Sayfası</h1>
+      <p>Burası kayıt olma sayfası olacak.</p>
+    </div>
+  );
+}
+
+export default RegisterPage;
